0cfc0082a2af6921f5f97a499df113d50b13dd54
wnascimento <weveron.sn@gmail.com>
2015-07-16 10:20:13 -0300

    tutorial laravel

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -5,25 +5,26 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request as Request;
 use App\models\Produto as Produto;
+use App\Http\Requests\ProdutoRequest as ProdutoRequest;
 
 class ProdutoController extends Controller
 {
 	
 	public function __construct(Produto $produto){
+		
 		$this->produto = $produto;
+	
 	}
 	
 	public function listar() 
 	{		
-		$produtos = $this->produto->all();
-		
+		$produtos = $this->produto->all();		
 		return view('produto.listagem')->with('produtos' , $produtos);
 	}
 
 	public function mostrar($id)
 	{		
 		$produto = $this->produto->find($id);
-		
 		return view('produto.detalhes')->with('produto', $produto);
 	}
 	
@@ -31,37 +32,39 @@ class ProdutoController extends Controller
 		return view('produto.formulario')->with('action','adicionar');
 	}
 	
-	public function adicionar(Request $request){
-		
+	public function adicionar(ProdutoRequest $request)
+	{		
 		$this->produto->create($request->all());
-		
 		return redirect()->action('ProdutoController@listar')->withInput($request->only('nome'));
 	}
 	
-	public function listaJson(){
-		
+	public function listaJson()
+	{
 		$produtos = $this->produto->all();
-		return response()->json($produtos);	
+		return response()->json($produtos);		
 	}
 	
-	public function remover($id){
+	public function remover($id)
+	{
 		$produto = $this->produto->find($id);
 		$produto->delete();
 		
 		return redirect()->action('ProdutoController@listar');
 	}
 	
-	public function editar($id){
+	public function editar($id)
+	{
 		$produto = $this->produto->find($id);
 	
 		return view("produto.formulario")->with('produto', $produto)->with('action','atualizar')->with('id',$id);
 	}
 	
-	public function atualizar(Request $request){
+	public function atualizar(ProdutoRequest $request){
 		
-		$produto = $request->except('_token');
+		$produtoAtual = $request->except('_token');		
+		$produto = $this->produto->find($produtoAtual['id']);
 		
-		$this->produto->updated($produto,$produto['id']);
+		$produto->update($produtoAtual);
 	
 		return redirect()->action('ProdutoController@listar');
 	}
--- resources/views/produto/formulario.blade.php
+++ resources/views/produto/formulario.blade.php
@@ -1,29 +1,38 @@
 @extends('layout.principal') @section('conteudo')
 
+@if(count($errors) > 0)
+<div class="alert alert-danger">
+	<ul>
+		@foreach($errors->all() as $erro)
+		<li>{{$erro}}</li>
+		@endforeach
+	</ul>
+</div>
+@endif
+
 <h1>Novo produto</h1>
 <form action="/produtos/{{$action}}" method="post">
-	<input type="hidden" value="{{{csrf_token()}}}" name="_token"/>
-	
-	<input type="hidden" value="{{$id or ''}}" name="id"/>
-	
+	<input type="hidden" value="{{{csrf_token()}}}" name="_token" /> <input
+		type="hidden" value="{{$id or ''}}" name="id" />
+
 	<div class="form-group">
-		<label for="nome">Nome:</label> 
-		<input type="text" class="form-control" name="nome" value="{{$produto->nome or ''}}"/>
+		<label for="nome">Nome:</label> <input type="text"
+			class="form-control" name="nome" value="{{$produto->nome or old('nome')}}" />
 	</div>
 	<div class="form-group">
-		<label>Descrição:</label> 
-		<input type="text" class="form-control" name="descricao" value="{{$produto->descricao or ''}}" />
+		<label>Descrição:</label> <input type="text" class="form-control"
+			name="descricao" value="{{$produto->descricao or old('descricao')}}" />
 	</div>
 	<div class="form-group">
-		<label>Valor:</label>  
-		<input type="text" class="form-control" name="valor" value="{{$produto->valor or ''}}" />
+		<label>Valor:</label> <input type="text" class="form-control"
+			name="valor" value="{{$produto->valor or old('valor')}}" />
 	</div>
 	<div class="form-group">
-		<label>Quantidade:</label> 
-		<input type="number" class="form-control" name="quantidade" value="{{$produto->quantidade or ''}}" />
+		<label>Quantidade:</label> <input type="number" class="form-control"
+			name="quantidade" value="{{$produto->quantidade or old('quantidade')}}" />
 	</div>
-	
-	<button type="submit" class="btn btn-primary btn-block" >Salvar</button>
+
+	<button type="submit" class="btn btn-primary btn-block">Salvar</button>
 </form>
 @stop
 
